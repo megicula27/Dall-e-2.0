98cf18a517fea1a21d5c9c0f5b2e78ff42dbb4d7
megicula27 <aditya1272001@gmail.com>
2024-03-12 17:25:49 +0530

    routes

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
+import express from "express";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
@@ -12,26 +12,30 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-router.route('/').get((req, res) => {
-  res.status(200).json({ message: 'Hello from DALL-E!' });
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Hello from DALL-E!" });
 });
 
-router.route('/').post(async (req, res) => {
+router.route("/").post(async (req, res) => {
   try {
     const { prompt } = req.body;
-
+    console.log("prompt:", prompt);
     const aiResponse = await openai.createImage({
       prompt,
       n: 1,
-      size: '1024x1024',
-      response_format: 'b64_json',
+      size: "1024x1024",
+      response_format: "b64_json",
     });
-
+    console.log("ai:", aiResponse);
     const image = aiResponse.data.data[0].b64_json;
+    console.log("Generated Image:", image);
     res.status(200).json({ photo: image });
   } catch (error) {
+    console.log("Error:", error);
     console.error(error);
-    res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+    res
+      .status(500)
+      .send(error?.response.data.error.message || "Something went wrong");
   }
 });
 
